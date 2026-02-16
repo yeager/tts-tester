fa3df4eee93d8914a2f27a7e8a001e9c87895e1b
BOSSe <BOSSe@danielnylander.se>
2026-02-16 09:58:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# ULText-to-speech engine comparison tool
+
+[![License: GPL-3.0](https://img.shields.io/badge/License-GPL%203.0-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+[![Transifex](https://img.shields.io/badge/Transifex-translate-green.svg)](https://www.transifex.com/danielnylander/tts-tester/)
+
+Text-to-speech engine comparison tool - a modern GTK4/Libadwaita application.
+
+## Features
+
+- Modern GTK4/Libadwaita interface
+- Multi-language support via Transifex
+- Keyboard shortcuts support
+- Cross-platform compatibility
+
+## Installation
+
+### From Source
+```bash
+git clone https://github.com/yeager/tts-tester.git
+cd tts-tester
+pip install -e .
+```
+
+### Debian/Ubuntu
+Download the latest `.deb` package from the [releases page](https://github.com/yeager/tts-tester/releases).
+
+### Fedora/RHEL
+Download the latest `.rpm` package from the [releases page](https://github.com/yeager/tts-tester/releases).
+
+## Usage
+
+Launch the application:
+```bash
+tts-tester
+```
+
+### Keyboard Shortcuts
+- `Ctrl+Q` - Quit application
+- `F5` - Refresh data
+- `Ctrl+?` - Show keyboard shortcuts
+
+## Translation
+
+Help translate this application on [Transifex](https://www.transifex.com/danielnylander/tts-tester/).
+
+## Author
+
+**Daniel Nylander**
+- Email: daniel@danielnylander.se
+- GitHub: [@yeager](https://github.com/yeager)
+
+## License
+
+This project is licensed under the GPL-3.0-or-later License - see the [LICENSE](LICENSE) file for details.
---
+++ src/tts_tester/main.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+
+import sys
+import gettext
+import gi
+
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
+
+from gi.repository import Gtk, Adw, Gio, GLib
+
+# Set up gettext
+TEXTDOMAIN = 'tts-tester'
+gettext.textdomain(TEXTDOMAIN)
+gettext.bindtextdomain(TEXTDOMAIN, '/usr/share/locale')
+_ = gettext.gettext
+
+class MainWindow(Adw.ApplicationWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.set_default_size(800, 600)
+        self.set_title(_(UMQTT))
+        
+        # Create header bar
+        header = Adw.HeaderBar()
+        self.set_titlebar(header)
+        
+        # Create main content
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.set_content(box)
+        
+        # Status bar
+        self.status_label = Gtk.Label(label=_("Ready"))
+        self.status_label.set_halign(Gtk.Align.START)
+        self.status_label.add_css_class("dim-label")
+        box.append(self.status_label)
+        
+    def show_about(self, action, param):
+        about = Adw.AboutWindow(transient_for=self)
+        about.set_application_name(_(UMQTT))
+        about.set_application_icon("se.danielnylander.tts-tester")
+        about.set_developer_name("Daniel Nylander")
+        about.set_version("0.1.0")
+        about.set_website("https://github.com/yeager/tts-tester")
+        about.set_translator_credits(_("translator-credits"))
+        about.present()
+        
+    def show_shortcuts(self, action, param):
+        builder = Gtk.Builder()
+        builder.add_from_string('''
+        <interface>
+          <object class="GtkShortcutsWindow" id="shortcuts">
+            <property name="modal">True</property>
+            <child>
+              <object class="GtkShortcutsSection">
+                <property name="section-name">shortcuts</property>
+                <child>
+                  <object class="GtkShortcutsGroup">
+                    <property name="title" translatable="yes">General</property>
+                    <child>
+                      <object class="GtkShortcutsShortcut">
+                        <property name="title" translatable="yes">Show Shortcuts</property>
+                        <property name="accelerator">&lt;Primary&gt;question</property>
+                      </object>
+                    </child>
+                    <child>
+                      <object class="GtkShortcutsShortcut">
+                        <property name="title" translatable="yes">Refresh</property>
+                        <property name="accelerator">F5</property>
+                      </object>
+                    </child>
+                    <child>
+                      <object class="GtkShortcutsShortcut">
+                        <property name="title" translatable="yes">Quit</property>
+                        <property name="accelerator">&lt;Primary&gt;q</property>
+                      </object>
+                    </child>
+                  </object>
+                </child>
+              </object>
+            </child>
+          </object>
+        </interface>
+        ''')
+        shortcuts = builder.get_object("shortcuts")
+        shortcuts.set_transient_for(self)
+        shortcuts.present()
+
+class Application(Adw.Application):
+    def __init__(self):
+        super().__init__(application_id="se.danielnylander.tts-tester")
+        
+    def do_activate(self):
+        window = self.props.active_window
+        if not window:
+            window = MainWindow(application=self)
+        window.present()
+        
+    def do_startup(self):
+        Adw.Application.do_startup(self)
+        
+        # Create actions
+        quit_action = Gio.SimpleAction.new("quit", None)
+        quit_action.connect("activate", self.quit_app)
+        self.add_action(quit_action)
+        
+        about_action = Gio.SimpleAction.new("about", None)
+        about_action.connect("activate", self.show_about)
+        self.add_action(about_action)
+        
+        shortcuts_action = Gio.SimpleAction.new("shortcuts", None)
+        shortcuts_action.connect("activate", self.show_shortcuts)
+        self.add_action(shortcuts_action)
+        
+        refresh_action = Gio.SimpleAction.new("refresh", None)
+        refresh_action.connect("activate", self.refresh_data)
+        self.add_action(refresh_action)
+        
+        # Set keyboard shortcuts
+        self.set_accels_for_action("app.quit", ["<Primary>q"])
+        self.set_accels_for_action("app.shortcuts", ["<Primary>question"])
+        self.set_accels_for_action("app.refresh", ["F5"])
+        
+    def quit_app(self, action, param):
+        self.quit()
+        
+    def show_about(self, action, param):
+        window = self.props.active_window
+        if window:
+            window.show_about(action, param)
+            
+    def show_shortcuts(self, action, param):
+        window = self.props.active_window
+        if window:
+            window.show_shortcuts(action, param)
+            
+    def refresh_data(self, action, param):
+        window = self.props.active_window
+        if window:
+            window.status_label.set_text(_("Refreshing..."))
+            # Add refresh logic here
+            GLib.timeout_add_seconds(1, lambda: window.status_label.set_text(_("Ready")))
+
+def main():
+    app = Application()
+    return app.run(sys.argv)
+
+if __name__ == '__main__':
+    main()
